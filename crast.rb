cc12164fc203f852326b4f10a19505d38fd7e365
Seif Kamal <seif@tickx.co.uk>
2020-03-22 11:20:49 +0000

    Add Crast formula

---
+++ crast.rb
@@ -0,0 +1,17 @@
+class Crast < Formula
+  desc "A command line, directory based todo list app."
+  homepage "https://github.com/safe-k/crast"
+  url "https://github.com/safe-k/crast/archive/v1.0.0.tar.gz"
+  sha256 "896b1957dd38dae52f1a5ce8b62f8e2a7b27d9ae43d74733fddfa447dc021cb9"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-o", "crast", "./cmd"
+    bin.install "crast"
+  end
+
+  test do
+    system "#{bin}/crast", "--help"
+  end
+end
