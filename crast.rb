e55f146866e74b9712573a7c470311d454336eb5
Seif Kamal <seif@tickx.co.uk>
2020-04-15 08:34:42 +0100

    Fix brew audit errors

--- crast.rb
+++ crast.rb
@@ -1,5 +1,5 @@
 class Crast < Formula
-  desc "A command line, directory based todo list app."
+  desc "Command-line, directory based todo list app"
   homepage "https://github.com/safe-k/crast"
   url "https://github.com/safe-k/crast/archive/v1.0.0.tar.gz"
   sha256 "896b1957dd38dae52f1a5ce8b62f8e2a7b27d9ae43d74733fddfa447dc021cb9"
